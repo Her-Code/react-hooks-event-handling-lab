9d49ea33af9efef59dcc1a4d597ca882802f8770
+Sharon <+wanjiku.kahira@gmail.com>
2024-08-01 15:53:04 +0300

    add event listener code to EyesOnMe.js,Keypad .js files

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,27 @@
 // Code Keypad Component Here
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+// function Keypad (){
+//     return (
+//         <div></div>
+//     )
+// }
+
+// export default Keypad;
+
+import React from 'react';
+
+function Keypad() {
+  const handleChange = () => {
+    console.log('Entering password...');
+  };
+
+  return (
+    <input
+      type="password"
+      onChange={handleChange}
+      placeholder="Enter your password"
+    />
+  );
 }
 
 export default Keypad;
